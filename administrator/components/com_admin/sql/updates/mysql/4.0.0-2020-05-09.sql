aa5e0a8a28fb9a50b9d009e4f2a048dc6e47a318
Artem Vasilev <kern.usr@gmail.com>
2020-05-09 21:13:52 +0300

    Convert metakey and metadesc columns from text to varchar(1024)

--- administrator/components/com_admin/sql/updates/mysql/4.0.0-2020-05-09.sql
+++ administrator/components/com_admin/sql/updates/mysql/4.0.0-2020-05-09.sql
@@ -1,4 +1,6 @@
-/* Add column for browser page title */
+--
+-- Add column for browser page title
+--
 ALTER TABLE `#__categories`
     ADD COLUMN `page_title` VARCHAR(1024) NOT NULL DEFAULT '' COMMENT 'Browser page title' COLLATE 'utf8mb4_unicode_ci' AFTER `params`;
 ALTER TABLE `#__tags`
@@ -9,3 +11,42 @@ ALTER TABLE `#__content`
     ADD COLUMN `page_title` VARCHAR(1024) NOT NULL DEFAULT '' COMMENT 'Browser page title' COLLATE 'utf8mb4_unicode_ci' AFTER `metakey`;
 ALTER TABLE `#__contact_details`
     ADD COLUMN `page_title` VARCHAR(1024) NOT NULL DEFAULT '' COMMENT 'Browser page title' COLLATE 'utf8mb4_unicode_ci' AFTER `metakey`;
+
+--
+-- Convert metakey and metadesc columns from text to varchar(1024)
+--
+UPDATE `#__contact_details`
+SET `metakey` = ''
+WHERE `metakey` IS NULL;
+
+UPDATE `#__contact_details`
+SET `metadesc` = ''
+WHERE `metadesc` IS NULL;
+
+ALTER TABLE `#__contact_details`
+    MODIFY `metakey` VARCHAR(1024) NOT NULL DEFAULT '' COLLATE 'utf8mb4_unicode_ci' AFTER `modified_by`,
+    MODIFY `metadesc` VARCHAR(1024) NOT NULL DEFAULT '' COLLATE 'utf8mb4_unicode_ci' AFTER `page_title`;
+
+UPDATE `#__content`
+SET `metakey` = ''
+WHERE `metakey` IS NULL;
+
+UPDATE `#__content`
+SET `metadesc` = ''
+WHERE `metadesc` IS NULL;
+
+ALTER TABLE `#__content`
+    MODIFY `metakey` VARCHAR(1024) NOT NULL DEFAULT '' COLLATE 'utf8mb4_unicode_ci' AFTER `ordering`,
+    MODIFY `metadesc` VARCHAR(1024) NOT NULL DEFAULT '' COLLATE 'utf8mb4_unicode_ci' AFTER `page_title`;
+
+UPDATE `#__newsfeeds`
+SET `metakey` = ''
+WHERE `metakey` IS NULL;
+
+UPDATE `#__newsfeeds`
+SET `metadesc` = ''
+WHERE `metadesc` IS NULL;
+
+ALTER TABLE `#__newsfeeds`
+    MODIFY `metakey` VARCHAR(1024) NOT NULL DEFAULT '' COLLATE 'utf8mb4_unicode_ci' AFTER `modified_by`,
+    MODIFY `metadesc` VARCHAR(1024) NOT NULL DEFAULT '' COLLATE 'utf8mb4_unicode_ci' AFTER `page_title`;
--- administrator/components/com_admin/sql/updates/postgresql/4.0.0-2020-05-09.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.0.0-2020-05-09.sql
@@ -1,3 +1,6 @@
+--
+-- Add column for browser page title
+--
 ALTER TABLE "#__categories"
     ADD COLUMN "page_title" VARCHAR(1024) NOT NULL DEFAULT '' COLLATE 'utf8mb4_unicode_ci';
 ALTER TABLE "#__tags"
@@ -8,3 +11,53 @@ ALTER TABLE "#__content"
     ADD COLUMN "page_title" VARCHAR(1024) NOT NULL DEFAULT '' COLLATE 'utf8mb4_unicode_ci';
 ALTER TABLE "#__contact_details"
     ADD COLUMN "page_title" VARCHAR(1024) NOT NULL DEFAULT '' COLLATE 'utf8mb4_unicode_ci';
+
+--
+-- Convert metakey and metadesc columns from text to varchar(1024)
+--
+UPDATE "#__contact_details"
+SET "metakey" = ''
+WHERE "metakey" IS NULL;
+
+UPDATE "#__contact_details"
+SET "metadesc" = ''
+WHERE "metadesc" IS NULL;
+
+ALTER TABLE "#__contact_details"
+    ALTER COLUMN "metakey" TYPE VARCHAR(1024) COLLATE 'utf8mb4_unicode_ci',
+    ALTER COLUMN "metakey" SET NOT NULL,
+    ALTER COLUMN "metakey" SET DEFAULT '',
+    ALTER COLUMN "metadesc" TYPE VARCHAR(1024) COLLATE 'utf8mb4_unicode_ci',
+    ALTER COLUMN "metadesc" SET NOT NULL,
+    ALTER COLUMN "metadesc" SET DEFAULT '';
+
+UPDATE "#__content"
+SET "metakey" = ''
+WHERE "metakey" IS NULL;
+
+UPDATE "#__content"
+SET "metadesc" = ''
+WHERE "metadesc" IS NULL;
+
+ALTER TABLE "#__content"
+    ALTER COLUMN "metakey" TYPE VARCHAR(1024) COLLATE 'utf8mb4_unicode_ci',
+    ALTER COLUMN "metakey" SET NOT NULL,
+    ALTER COLUMN "metakey" SET DEFAULT '',
+    ALTER COLUMN "metadesc" TYPE VARCHAR(1024) COLLATE 'utf8mb4_unicode_ci',
+    ALTER COLUMN "metadesc" SET NOT NULL,
+    ALTER COLUMN "metadesc" SET DEFAULT '';
+UPDATE "#__newsfeeds"
+SET "metakey" = ''
+WHERE "metakey" IS NULL;
+
+UPDATE "#__newsfeeds"
+SET "metadesc" = ''
+WHERE "metadesc" IS NULL;
+
+ALTER TABLE "#__newsfeeds"
+    ALTER COLUMN "metakey" TYPE VARCHAR(1024) COLLATE 'utf8mb4_unicode_ci',
+    ALTER COLUMN "metakey" SET NOT NULL,
+    ALTER COLUMN "metakey" SET DEFAULT '',
+    ALTER COLUMN "metadesc" TYPE VARCHAR(1024) COLLATE 'utf8mb4_unicode_ci',
+    ALTER COLUMN "metadesc" SET NOT NULL,
+    ALTER COLUMN "metadesc" SET DEFAULT '';
