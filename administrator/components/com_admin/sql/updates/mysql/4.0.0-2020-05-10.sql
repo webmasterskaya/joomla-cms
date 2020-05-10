31c3ce80a62060f0d0dfaa8b83755ed8a64c1acc
Artem Vasilev <kern.usr@gmail.com>
2020-05-10 13:45:48 +0300

    Move article page title from attribs to a separate field

---
+++ administrator/components/com_admin/sql/updates/mysql/4.0.0-2020-05-10.sql
@@ -0,0 +1,7 @@
+--
+-- Move article page title from attribs to a separate field
+--
+UPDATE `#__content`
+SET `page_title` = `attribs` ->> '$.article_page_title'
+WHERE `page_title` = ''
+  AND `attribs` ->> '$.article_page_title' <> ''
